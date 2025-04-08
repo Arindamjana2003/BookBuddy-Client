5c763ac71c2be5b47bd60def76533a60665f5bd8
Sumana Hait <sumanahait06@gmail.com>
2025-04-08 14:07:21 +0530

    timedescriptor created @sumanahait

--- utils/TimeDescriptor.ts
+++ utils/TimeDescriptor.ts
@@ -1,7 +1,17 @@
 export const TimeDescriptor = () => {
-  const time = new Date().getHours();
-
+  const time = new Date().getHours();  
   console.log(time);
 
-  return 'Good Morninng';
-};
+  
+  if (time >= 5 && time < 12) {
+    return 'Good Morning';  
+  } else if (time >= 12 && time < 16) {
+    return 'Good Noon';     
+  } else if (time >= 16 && time < 18) {
+    return 'Good Afternoon';  
+  } else if (time >= 18 && time < 20) {
+    return 'Good Evening';    
+  } else {
+    return 'Good Night';     
+  }
+}
