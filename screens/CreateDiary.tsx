d24521d5047c0888f76f5253bb598fde86a135e0
Pritam <123.paulpritam@gmail.com>
2025-04-09 16:18:44 +0530

    changes

---
+++ screens/CreateDiary.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const CreateDiary = () => {
+  return (
+    <View>
+      <Text>CreateDiary</Text>
+    </View>
+  );
+};
+
+export default CreateDiary;
+
+const styles = StyleSheet.create({});
