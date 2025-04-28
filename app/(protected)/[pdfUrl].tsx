a29e59a411deb0d3bea13f988ec69dda033673dc
Pritam <123.paulpritam@gmail.com>
2025-04-28 12:06:35 +0530

    add

--- app/(protected)/[pdfUrl].tsx
+++ app/(protected)/[pdfUrl].tsx
@@ -6,43 +6,43 @@ import {
   ActivityIndicator,
   useColorScheme,
 } from 'react-native';
-// import React, { useState } from 'react';
-// import { useLocalSearchParams } from 'expo-router';
-// import Pdf from 'react-native-pdf';
-// import { Colors } from '@/constants/Colors';
+import React, { useState } from 'react';
+import { useLocalSearchParams } from 'expo-router';
+import Pdf from 'react-native-pdf';
+import { Colors } from '@/constants/Colors';
 
 const PdfUrl = () => {
-  // const { pdfUrl }: any = useLocalSearchParams();
-  // const [loading, setLoading] = useState<boolean>(true); // state for loading indicator
-  // const [error, setError] = useState<string | null>(null); // state to manage errors
+  const { pdfUrl }: any = useLocalSearchParams();
+  const [loading, setLoading] = useState<boolean>(true); // state for loading indicator
+  const [error, setError] = useState<string | null>(null); // state to manage errors
 
-  // const theme = Colors[useColorScheme() ?? 'light'];
+  const theme = Colors[useColorScheme() ?? 'light'];
 
-  // const source = {
-  //   uri: pdfUrl,
-  //   cache: true,
-  // };
+  const source = {
+    uri: pdfUrl,
+    cache: true,
+  };
 
-  // const handleLoadComplete = (numberOfPages: any, filePath: any) => {
-  //   console.log(`Number of pages: ${numberOfPages}`);
-  //   setLoading(false); // Hide loading indicator once PDF is loaded
-  // };
+  const handleLoadComplete = (numberOfPages: any, filePath: any) => {
+    console.log(`Number of pages: ${numberOfPages}`);
+    setLoading(false); // Hide loading indicator once PDF is loaded
+  };
 
-  // const handleError = (error: any) => {
-  //   console.log(error);
-  //   setLoading(false);
-  //   setError('Failed to load PDF. Please try again later.');
-  // };
+  const handleError = (error: any) => {
+    console.log(error);
+    setLoading(false);
+    setError('Failed to load PDF. Please try again later.');
+  };
 
   return (
     <View style={styles.container}>
-      {/* {loading && (
+      {loading && (
         <ActivityIndicator size="large" color={theme.primary} style={styles.loadingIndicator} />
       )}
 
-      {error && <Text style={styles.errorText}>{error}</Text>} */}
+      {error && <Text style={styles.errorText}>{error}</Text>}
 
-      {/* <Pdf
+      <Pdf
         source={source}
         onLoadComplete={handleLoadComplete}
         onPageChanged={(page, numberOfPages) => {
@@ -54,7 +54,7 @@ const PdfUrl = () => {
         }}
         style={[styles.pdf, { backgroundColor: theme.background }]}
         enablePaging={true}
-      /> */}
+      />
     </View>
   );
 };
